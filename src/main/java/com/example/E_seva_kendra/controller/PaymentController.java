5292e69b9652453ba21f4d70cbe1ef8b73025252
Nitin204 <nitinkatore686@gmail.com>
2026-03-06 14:58:48 +0530

    Added order  API and payment fixes

---
+++ src/main/java/com/example/E_seva_kendra/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.example.E_seva_kendra.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.E_seva_kendra.model.Payment;
+import com.example.E_seva_kendra.service.service.PaymentService;
+
+@RestController
+@RequestMapping("/api/payment")
+@CrossOrigin("*")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    // Save Payment
+    @PostMapping("/confirm")
+    public String confirmPayment(
+            @RequestParam String name,
+            @RequestParam String mobile,
+            @RequestParam String serviceName,
+            @RequestParam String extraData,
+            @RequestParam Double amount,
+            @RequestParam("screenshot") MultipartFile screenshot){
+
+        return paymentService.confirmPayment(name, mobile, serviceName, extraData, amount, screenshot);
+    }
+
+    // Get All Payments (Admin Panel)
+    @GetMapping
+    public List<Payment> getAllPayments(){
+        return paymentService.getAllPayments();
+    }
+}
---
+++ src/main/java/com/example/E_seva_kendra/dto/OrderRequest.java
@@ -0,0 +1,47 @@
+package com.example.E_seva_kendra.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderRequest {
+
+    private String name;
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public String getExtraData() {
+		return extraData;
+	}
+
+	public void setExtraData(String extraData) {
+		this.extraData = extraData;
+	}
+
+	private String mobile;
+
+    private String serviceName;
+
+    private String extraData;
+}
